d61c313b34b4ab99dca9b876b89e2be2c49738ab
k8everett <k8.everett@gmail.com>
2021-03-27 16:31:37 -0400

    Created ShapesListImpl/ShapesListImplTest

---
+++ src/cs5004/animator/ShapesListImpl.java
@@ -0,0 +1,23 @@
+package cs5004.animator;
+
+public class ShapesListImpl implements ShapesList {
+  @Override
+  public void addShape(Shape shape, String identifier) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void move(Shape shape, double x, double y, int start, int stop) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void changeColor(Shape shape, double red, double blue, double green) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void scale(Shape shape, double scalar) throws IllegalArgumentException {
+
+  }
+}
