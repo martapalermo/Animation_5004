661edbe436586dd2b9a1a6003751877afcede12f
martapalermo <77807333+martapalermo@users.noreply.github.com>
2021-03-30 21:53:48 -0400

    added rectangle toString

--- src/cs5004/animator/Rectangle.java
+++ src/cs5004/animator/Rectangle.java
@@ -1,13 +1,20 @@
 package cs5004.animator;
 
 public class Rectangle extends AbstractShape {
-  public Rectangle(double x, double y, int appearTime, int disappearTime, double width, double height, double red, double blue, double green) throws IllegalArgumentException {
+  public Rectangle(double x, double y, int appearTime, int disappearTime, double width,
+                   double height, double red, double blue, double green)
+      throws IllegalArgumentException {
     super(x, y, appearTime, disappearTime, width, height, red, blue, green);
   }
 
   // Format like in the spec
   @Override
   public String toString() {
-    return super.toString();
+    return "Name: " + this.getName()
+        + "Type: rectangle\n" + "Min corner: (" + this.getX() + ","
+        + this.getY() + "), " + "Width: " + getWidth() + ", " + "Height: " + getHeight() + ", "
+        + "Color: (" + this.getRed() + "," + this.getGreen() + "," + this.getBlue() + ")\n"
+        + "Appears at t=" + getAppearTime() + "\n"
+        + "Disappears at t=" + getDisappearTime() + "\n";
   }
 }
