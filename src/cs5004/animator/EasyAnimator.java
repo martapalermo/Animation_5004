fb6101832741efee9db8760a88cbfe62f40d98fb
k8everett <k8.everett@gmail.com>
2021-04-06 12:42:55 -0400

    main method

---
+++ src/cs5004/animator/EasyAnimator.java
@@ -0,0 +1,10 @@
+package cs5004.animator;
+
+public final class EasyAnimator {
+
+  public static void main(String[] args) {
+    for (String arg : args) {
+      System.out.println(arg);
+    }
+  }
+}
