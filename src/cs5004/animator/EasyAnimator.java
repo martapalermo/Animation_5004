ee988a79bcc6f2a6c114337ce38dadafa31762d1
k8everett <k8.everett@gmail.com>
2021-04-06 17:22:47 -0400

    main method

--- src/cs5004/animator/EasyAnimator.java
+++ src/cs5004/animator/EasyAnimator.java
@@ -1,10 +1,64 @@
 package cs5004.animator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.*;
+
 public final class EasyAnimator {
 
-  public static void main(String[] args) {
+  public static void parseCommands(String[] args) throws IllegalArgumentException {
+//    String[] argSplit = Arrays.toString(args).split("-");
+//    List<String> argList = Arrays.asList(argSplit);
+//    System.out.println(Arrays.toString(args).substring(1, args.length - 1));
+    StringBuilder strArgs = new StringBuilder();
+
     for (String arg : args) {
-      System.out.println(arg);
+      strArgs.append(arg).append(" ");
     }
+
+    Scanner scan = new Scanner(String.valueOf(strArgs));
+
+    // [in, view, out, speed]
+    String[] input = new String[4];
+
+    while (scan.hasNext()) {
+      String token = scan.next();
+
+      if (token.charAt(0) == '-') {
+        String command = scan.next();
+        if (token.equalsIgnoreCase("-in")) {
+
+          // Exception if the file doesn't exist?
+          if (!command.endsWith(".txt")) {
+            JOptionPane.showMessageDialog(null, "Invalid input file.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+          }
+          input[0] = command;
+        }
+
+        else if (token.equalsIgnoreCase("-view")) {
+          if (!command.equalsIgnoreCase("text") && !command.equalsIgnoreCase
+                  ("visual") && !command.equalsIgnoreCase("svg")) {
+            JOptionPane.showMessageDialog(null, "Invalid view type.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+          }
+        }
+      }
+    }
+
+//    if (!argList.contains("-in")) {
+//      throw new IllegalArgumentException("Must provide an input file.");
+//    }
+//
+//    if (!argList.contains("-view")) {
+//      throw new IllegalArgumentException("Must provide a view.");
+//    }
+  }
+
+  public static void main(String[] args) {
+    parseCommands(args);
   }
 }
