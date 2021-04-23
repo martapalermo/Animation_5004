45d6a8afacf140365d753736bec8bd2ae65dc399
martapalermo <77807333+martapalermo@users.noreply.github.com>
2021-04-23 16:42:54 -0400

    reformatted post handing + added javadoc + cleaned comments

--- src/cs5004/animator/controller/InteractiveController.java
+++ src/cs5004/animator/controller/InteractiveController.java
@@ -4,17 +4,40 @@ package cs5004.animator.controller;
  * Controller interface for the visual view specifically/ the interactive view.
  */
 public interface InteractiveController extends Controller {
-  public void start();
 
-  public void stop();
+  /**
+   * Method that assigns the specific action for start button.
+   */
+  void start();
 
-  public void restart();
+  /**
+   * Method that assigns the specific action for stop/pause button.
+   */
+  void stop();
 
-  public void loop();
+  /**
+   * Method that assigns the specific action for restart/resume button.
+   */
+  void restart();
 
-  public void speedUp();
+  /**
+   * Method that assigns the specific action for loop checkbox.
+   */
+  void loop();
 
-  public void slowDown();
+  /**
+   * Method that assigns the specific action for speed up button.
+   */
+  void speedUp();
 
-  public String getLog();
+  /**
+   * Method that assigns the specific action for slow down button.
+   */
+  void slowDown();
+
+  /**
+   * Getter method for the button logs.
+   * @return returns the button click logged activity, String
+   */
+  String getLog();
 }
--- src/cs5004/animator/view/InteractiveView.java
+++ src/cs5004/animator/view/InteractiveView.java
@@ -9,11 +9,26 @@ import cs5004.animator.controller.InteractiveController;
  */
 public interface InteractiveView extends VisualView {
 
+  /**
+   * Helper method that sets buttons to their respective action- methods.
+   * @param controller interactiveController interface
+   */
   void setListeners(InteractiveController controller);
 
+  /**
+   * Setter method for speed.
+   * @param speed speed of animation, int
+   */
   void setSpeed(int speed);
 
+  /**
+   * Getter method for endTime for animation.
+   * @return  endTime, int
+   */
   void run(int count);
 
+  /**
+   * Setter method for endTime.
+   */
   int getEndTime();
 }
